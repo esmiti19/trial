f2b849451dc2de49352af3bda08cee15a64ae0c2
esmiti19 <gbesme21@gmail.com>
2023-06-22 11:43:43 +0300

    new commited to local R

---
+++ cheak.java
@@ -0,0 +1,9 @@
+public class cheak {
+ public static void main(String[] args) {
+    
+  System.out.print("i am legend");
+  System.out.print("i am legend");
+  System.out.print("i am legend");
+  
+}
+}
